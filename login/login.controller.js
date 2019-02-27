daed8ca8a7b5c7111dd0147c552be8df65aad8e0
FRSCO <frscollo@hotmail.it>
2019-02-27 19:58:43 +0000

    Add new register login

--- login/login.controller.js
+++ login/login.controller.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app')
+        .module('App')
         .controller('LoginController', LoginController);
 
     LoginController.$inject = ['$location', 'AuthenticationService', 'FlashService'];
--- register/register.controller.js
+++ register/register.controller.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app')
+        .module('App')
         .controller('RegisterController', RegisterController);
 
     RegisterController.$inject = ['UserService', '$location', '$rootScope', 'FlashService'];
